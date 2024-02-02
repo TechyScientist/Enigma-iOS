4004e9c49b691645f8f20844847415218d8d967e
Johnny Console <johnny.console@hotmail.com>
2024-02-02 10:22:05 -0500

    Add Main Storyboard Layout and Outlet Collections for Lamps and Keys

--- Enigma-iOS/ViewController.swift
+++ Enigma-iOS/ViewController.swift
@@ -7,11 +7,40 @@
 
 import UIKit
 
-class ViewController: UIViewController {
-
+class ViewController: UIViewController, UIPickerViewDelegate, UIPickerViewDataSource {
+    
+    @IBOutlet private weak var rotorPicker: UIPickerView!
+    @IBOutlet private var lamps: [UILabel]!
+    @IBOutlet private var keys: [UIButton]!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rotorPicker.selectRow(1, inComponent: 1, animated: false)
+        rotorPicker.selectRow(2, inComponent: 2, animated: false)
+    }
+    
+    func numberOfComponents(in pickerView: UIPickerView) -> Int {
+        return 3
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, numberOfRowsInComponent component: Int) -> Int {
+            return 5
+    }
+    
+    func pickerView(_ pickerView: UIPickerView, titleForRow row: Int, forComponent component: Int) -> String? {
+        switch(row) {
+            case 0:
+                return "I"
+            case 1:
+                return "II"
+            case 2:
+                return "III"
+            case 3:
+                return "IV"
+            default:
+                return "V"
+        }
     }
 
 
